595fc89cc5cccfa97fbe77bf1fffc8cd965087eb
Paulo Hercilio Araujo Santana <phasantana@sedurbs.se>
2022-07-11 12:17:55 -0300

    terminando upload e adcionando lida de arquivos csv

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,20 @@
+import fs from "fs";
+import { parse } from "csv-parse";
 import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: CategoriesRepository) {}
-  execute(file: Express.Multer.File ) : void {
 
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
